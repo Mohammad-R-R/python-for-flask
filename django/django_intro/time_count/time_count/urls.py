472dcbf8c354f43d7b141849b11534ddbfac4365
mohammadraddad <d.dodo.dode@hotmail.com>
2021-05-18 12:29:02 +0300

    with moath

--- django/django_intro/time_count/time_count/urls.py
+++ django/django_intro/time_count/time_count/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.urls import path, include           # import include
 # from django.contrib import admin              # comment out, or just delete
 urlpatterns = [
+    path('', include('dojo_survey.urls')),
     path('', include('time_app.urls')),
+    
     # path('admin/', admin.sites.urls)         # comment out, or just delete
 ]
